5801349a1ff776bfa1b69a693b5016b735c484f2
Mehetab Ali <shell0bee0@gmail.com>
2024-07-23 03:23:39 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,10 @@
+console.log(typeof null);
+
+// numbers
+// String
+// boolean true/false
+// bigint bahut jyada bada number 
+// symbol  = for uniqness
+// null is null empty
+// undefined is not assignes value
+// lastly object bhi kuch hota hai will see it further ahead
