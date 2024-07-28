72ee8656ab9ff53096d83314b728e9c3ac958c8e
Mehetab Ali <shell0bee0@gmail.com>
2024-07-28 08:52:21 +0000

    revised-basics

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -8,3 +8,33 @@ console.log(typeof null);
 // null is null empty
 // undefined is not assignes value
 // lastly object bhi kuch hota hai will see it further ahead
+
+
+
+/* 
+ datatypes 2 hote hai
+
+ primitive
+
+ string 
+ boolean
+ bigint
+ symbol
+ null
+ undefined
+
+ non-primitive / reference
+
+ arrays
+ object
+ function
+
+
+ let name = new String("Shell0");
+ console.log(name);
+ 
+ ye wale let se string array ke form mai ata hai
+ phir proto karke ek hota uske baad to lowercase to uppercase use kar te hai
+ 
+ 
+ */
