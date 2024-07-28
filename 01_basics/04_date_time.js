661df5250ef2fb1c118f00bacc8b8725037ac0d3
Mehetab Ali <shell0bee0@gmail.com>
2024-07-28 11:47:07 +0000

    date and time

---
+++ 01_basics/04_date_time.js
@@ -0,0 +1,26 @@
+// let meraWakt = new Date();
+// console.log(meraWakt);
+// console.log(meraWakt.toString());
+// console.log(meraWakt.toDateString());
+// console.log(meraWakt.toISOString());
+// console.log(meraWakt.toLocaleString());
+// console.log(meraWakt.toLocaleDateString());
+// console.log(meraWakt.toLocaleTimeString());
+// console.log(meraWakt.toJSON());
+// console.log(meraWakt.toUTCString());
+// console.log(meraWakt.getDate());
+// console.log(meraWakt.getFullYear());
+// console.log(meraWakt.set);
+
+// let newTime = Date.now();
+// console.log(newTime);
+
+
+
+
+let indTime = new Date();
+console.log(indTime.toLocaleString(
+    "en-IN" , {timeZone: "Asia/Kolkata"}
+
+));
+
