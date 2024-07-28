c1f33e5edec6c7749c00b6a71d181574f24cf9c4
Mehetab Ali <shell0bee0@gmail.com>
2024-07-28 12:47:25 +0000

    arrays_part_1

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,22 @@
+let myArr = ["Mehetab", "Shell", "Jack", "dwd2"];
+// console.log(myArr.slice(1, 3));               // ismai bas e print hota hai
+
+// console.log(myArr);
+// myArr.unshift(4);                             // isse 0th index mai add ho jata hai 
+// console.log(myArr);
+// myArr.shift();                   // isse 0th index wala nikal jata hai
+
+// console.log(myArr);
+// myArr.push("Me");                // ismai last mai add hota hai
+// console.log(myArr);
+// myArr.pop();                     // ismai last se pop out ho jata hai
+// console.log(myArr);
+
+// console.log(myArr.splice(1, 3));              // ye wala refrence se cut ho jata hai 
+// console.log(myArr);
+
+
+
+
+let newArr = new Array (200,8,4,2,51,845,74,86,5,94,21);
+console.log(newArr.splice(3,5));        // here 3 is position to start & 5 is the No. of delete count
