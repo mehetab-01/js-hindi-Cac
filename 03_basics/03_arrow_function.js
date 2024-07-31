bc9b324ce4786019b7c647a8d1aeb8afab0b1b25
Mehetab Ali <shell0bee0@gmail.com>
2024-07-31 16:42:21 +0000

    arrow_function

---
+++ 03_basics/03_arrow_function.js
@@ -0,0 +1,48 @@
+const user = {
+    username: "hitesh",
+    price: 999,
+    welcomeMessage: function() {
+        console.log(`${this.username} , welcome to website`);
+        console.log(this);
+    }
+}
+
+// user.welcomeMessage()
+// user.username = "sam"
+// user.welcomeMessage()
+
+// console.log(this);                  //global scpoe mai defined jo jo hai
+
+function chai(){
+    let username = "hitesh"
+    console.log(this.username);
+}
+
+chai()
+
+// const chai = function () {
+//     let username = "hitesh"
+//     console.log(this.username);
+// }
+
+const chai2 =  () => {
+    let username = "hitesh"
+    console.log(this);
+}
+
+
+// chai()
+
+// const addTwo = (num1, num2) => {
+//     return num1 + num2
+// }
+
+let addTwo2 = (num1, num2) =>  num1 + num2
+let add2Two = (num1, num2) => ( num1 + num2 )
+const addTwo = (num1, num2) => ({username: "hitesh"})
+
+console.log(addTwo(3, 4))
+
+const myArray = [2, 5, 3, 7, 8]
+
+myArray.forEach(() => {})
